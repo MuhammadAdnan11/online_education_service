2bc5373008d21a319e4ef7c669b1a4d5b90a6a08
Samiullah191014 <pakistanarmykosalaam@gmail.com>
2023-02-15 15:46:24 +0500

    navbar updated file

--- project/src/Components/Navbar.js
+++ project/src/Components/Navbar.js
@@ -1,125 +1,206 @@
-import React from "react"
+import React from "react";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { RiAdminLine } from "react-icons/ri";
+import { BsPencil } from "react-icons/bs";
+import { VscSearch } from "react-icons/vsc";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { GoThreeBars } from "react-icons/go";
 
+import { AiOutlineHome } from "react-icons/ai";
+import { MdOutlineMedicalServices } from "react-icons/md";
+import { BiBookReader } from "react-icons/bi";
+import { AiOutlineInsertRowAbove } from "react-icons/ai";
 
-const Navbar=()=>{
-
-    return(
-        <>
-
-  <div className="topbar ">
-    <div className="container">
-      <div className="row">
-        <div className="col-xl-6 align-self-center">
-          <div className="logo text-end"> <a href="#" className="text-white ">
-          <img src="./Images/Oes Logo/logo2.jpeg" alt="" style={{width:200,height:70 }}/>
-          </a></div>
-        </div>
-
-
-        <div className="col-xl-6 align-self-center">
-          <div className="toggle text-end">
-           
-            <a className="btn  fw-bold fs-4 text-black" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-            <i className="fa fa-bars"></i>
-           </a>
+const Navbar = () => {
+  return (
+    <>
+      <div className="container-fluid mobile-toggler d-none d-xl-block ">
+        <div className="row p-3 ">
+          <div className="col d-flex flex-row gap-3 align-self-center justify-content-center ">
+            <h6>WELCOME </h6>
+            <h6>
+              {" "}
+              CALL <span className="iconcolor1">+44 300 303 0266</span>
+            </h6>
+            <h6>FOLLOW US</h6>
+            <AiOutlineTwitter className="iconcolor1" />
+            <FaInstagramSquare className="iconcolor1" />
+            <FaFacebookF className="iconcolor1" />
+          </div>
+          <div className="col d-flex namargin flex-row justify-content-end ">
+            <button
+              type="button"
+              className="btn btn-col btn-sm text-white rounded-pill"
+            >
+              <RiAdminLine className="iconcolor" /> Login
+            </button>
+            <button
+              type="button"
+              className="btn btn-col btn-sm text-white rounded-pill"
+            >
+              <BsPencil className="iconcolor" /> Register
+            </button>
           </div>
         </div>
-
       </div>
-    </div>
-  </div>
 
+      <header>
+        <div className="container-fluid">
+          <div className="navb-logo ">
+            <img src="./Images/Oes Logo/logo7.jpeg" alt="Logo" />
+          </div>
 
-  <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-  <div className="offcanvas-header">
-    <h5 className="offcanvas-title" id="offcanvasExampleLabel">
-     <span className="text-primary fs-4">Online Education Service</span> 
-     </h5>
-    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div className="offcanvas-body">
-   
-    <div className="dropdown mt-3">
-      <button className="btn btn-info rounded-pill p-2 dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-        Home
-      </button>
-      <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a className="dropdown-item" href="#">Action</a></li>
-        <li><a className="dropdown-item" href="#">Another action</a></li>
-        <li><a className="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-       <br/><br/>
+          <div className="navb-items d-none d-xl-flex">
+            <div className="item">
+              <a href="/">HOME</a>
+            </div>
 
+            <div className="item">
+              <a href="/services">COURSES</a>
+            </div>
 
-      <button className="btn btn-info rounded-pill p-2 dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-        Courses
-      </button>
-      <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a className="dropdown-item" href="#">Features</a></li>
-        <li><a className="dropdown-item" href="#">Categories</a></li>
-       
-      </ul>
-      <br/><br/>
+            <div className="item">
+              <a href="/cases">INSTRUCTORS</a>
+            </div>
 
-      <button className="btn btn-info rounded-pill p-2 dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-        Instructors
-      </button>
-      <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a className="dropdown-item" href="#">Become an instructor</a></li>
-        <li><a className="dropdown-item" href="#">Instructor Single</a></li>
-        
-      </ul>
-      <br/><br/>
+            <div className="item">
+              <a href="/about">EVENTS</a>
+            </div>
 
-      <button className="btn btn-info rounded-pill p-2 dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-        Events
-      </button>
-      <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a className="dropdown-item" href="#">Events Calendar</a></li>
-        <li><a className="dropdown-item" href="#">Event List</a></li>
-       
-      </ul>
-      <br/><br/>
+            <div className="item">
+              <a href="/about">PAGES</a>
+            </div>
 
-      <button className="btn btn-info rounded-pill p-2 dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-        Pages
-      </button>
-      <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a className="dropdown-item" href="#">About Us</a></li>
-        <li><a className="dropdown-item" href="#">Pricing Plans</a></li>
-        <li><a className="dropdown-item" href="#">Gallery</a></li>
-        <li><a className="dropdown-item" href="#">FAQ Page</a></li>
-        <li><a className="dropdown-item" href="#">Contact Us</a></li>
-        <li><a className="dropdown-item" href="#">404 Error Page</a></li>
+            <div className="item">
+              <a href="/about">ELEMENTS</a>
+            </div>
 
-      </ul>
-      <br/>
-      <br/>
+            <div className="item">
+              <a href="/about">
+                <VscSearch />
+              </a>
+            </div>
 
-      <button className="btn btn-info rounded-pill p-2 dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-        Elements
-      </button>
-      <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a className="dropdown-item" href="#">Action</a></li>
-        <li><a className="dropdown-item" href="#">Another action</a></li>
-        <li><a className="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+            <div className="item">
+              <a href="/about">
+                <AiOutlineShoppingCart />
+              </a>
+            </div>
 
+            <button
+              className="btn item"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasRight"
+              aria-controls="offcanvasRight"
+            >
+              <GoThreeBars />
+            </button>
 
+            <div
+              className="offcanvas offcanvas-end"
+              tabindex="-1"
+              id="offcanvasRight"
+              aria-labelledby="offcanvasRightLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 id="offcanvasRightLabel">Online Education Service</h5>
+                <button
+                  type="button"
+                  className="btn-close text-reset"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body">
+                Lorem ipsum dolor sit amet, conse ctetur adip elit, pellentesque
+                turpis.
+                <br />
+                <br />
+                <br />
+                <br />
+                <AiOutlineTwitter className="iconcolor2" />
+                <FaInstagramSquare className="iconcolor2" />
+                <FaFacebookF className="iconcolor2" />
+              </div>
+            </div>
+          </div>
 
+          {/* Button trigger modal  */}
+          <div className="mobile-toggler d-lg-none">
+            <a href="#" data-bs-toggle="modal" data-bs-target="#navbModal">
+              <GoThreeBars />
+            </a>
+          </div>
 
+          {/* Modal */}
+          <div
+            className="modal fade"
+            id="navbModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <img src="./Images/Oes Logo/logo7.jpeg" alt="logo" />
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <i className="fa-solid fa-xmark"></i>
+                  </button>
+                </div>
 
+                <div className="modal-body">
+                  <div className="modal-line">
+                    <AiOutlineHome classNameName="fs-1 colo " />
+                    <a href="/">Home</a>
+                  </div>
 
+                  <div className="modal-line">
+                    <MdOutlineMedicalServices classNameName="fs-1 colo " />
+                    <a href="/services">Services</a>
+                  </div>
 
+                  <div className="modal-line">
+                    <BiBookReader classNameName="fs-1 colo  " />
+                    <a href="/cases">Courses</a>
+                  </div>
 
+                  <div className="modal-line">
+                    <AiOutlineInsertRowAbove classNameName="fs-1 colo " />
+                    <a href="/about">About</a>
+                  </div>
 
+                  <a href="/contact" className="navb-button" type="button">
+                    Let's talk
+                  </a>
+                </div>
 
+                <div className="mobile-modal-footer">
+                  <a target="_blank" href="#">
+                    <AiOutlineTwitter className="iconcolor1" />
+                  </a>
+                  <a target="_blank" href="#">
+                    <FaInstagramSquare className="iconcolor1" />
+                  </a>
+                  <a target="_blank" href="#">
+                    <FaFacebookF className="iconcolor1" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
