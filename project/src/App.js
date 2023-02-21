f98936f72367d25fa9d74f0a3a5c2bde2dc21efe
MuhammadAdnan11 <adnan191022@gmail.com>
2023-02-21 16:39:02 +0500

    Faq page added

--- project/src/App.js
+++ project/src/App.js
@@ -2,8 +2,11 @@
 import React from 'react';
 // import Navbar from './Components/Navbar';
 // import Home from "./Components/Home"
-import Gallery from './Components/Gallery';
+// import Gallery from './Components/Gallery';
 // import Error_page from './Components/error_page';
+
+import Faq from './Components/Faq';
+
 import './App.css';
 
 
@@ -13,8 +16,9 @@ function App() {
    
 {/* <Navbar/>
 <Home/> */}
-      <Gallery/>
- {/* <Error_page/> */}
+      {/* <Gallery/> */}
+      {/* <Error_page/> */}
+      <Faq/>
    </>
   );
 }
