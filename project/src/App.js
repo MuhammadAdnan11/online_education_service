ba145912606cd151067e432a582db8fc2c96a25a
MuhammadAdnan11 <adnan191022@gmail.com>
2023-02-25 09:32:23 +0500

    Updated

--- project/src/App.js
+++ project/src/App.js
@@ -2,13 +2,13 @@
 import React from 'react';
 // import Navbar from './Components/Navbar';
 // import Home from "./Components/Home"
-// import Gallery from './Components/Gallery';
+import Gallery from './Components/Gallery';
 // import Error_page from './Components/error_page';
 
 // import Faq from './Components/Faq';
 // import Contact from './Components/contactUs';
 // import Cart from './Components/cart';
-import Dashboard from './Components/dashboard';
+// import Dashboard from './Components/dashboard';
 import './App.css';
 
 
@@ -18,12 +18,12 @@ function App() {
    
 {/* <Navbar/>
 <Home/> */}
-      {/* <Gallery/> */}
+      <Gallery/>
       {/* <Error_page/> */}
       {/* <Faq /> */}
       {/* <Contact/> */}
       {/* <Cart /> */}
-      <Dashboard/>
+      {/* <Dashboard/> */}
   </>    
   );
 }
--- project/src/Components/Gallery.js
+++ project/src/Components/Gallery.js
@@ -4,29 +4,30 @@ import "./gallery.css";
 const Gallery=()=> {
   return (
     <>
-      <div className='container-fluid imgbg'>
-        <div className='container'>
-          <div className='row text-sm-center'>
-       <h1 className='text-white mt-5 p-4'> Gallery </h1>
+       <div className='container-fluid  imgbg '>
+                <div className='container'>
+                    <div className='row'>
+                    <h1 className='fw-bolder text-white mt-5 pt-3  hcard' >Gallery</h1>
+                    </div>
+                    
+                </div>
+         
+            </div>
 
-          </div>
-       </div>
-      </div>
-
-      <div className='container mt-5'>
-        <div className='row d-flex flex-row gap-2 justify-content-center cursor-pointer HoverDiv'>
-          <img src="./Images/Gallery/gallery-img-1.jpg" alt="" style={{ width: 250, height: 200 }}></img>
-          <img src="./Images/Gallery/gallery-img-2.jpg" alt="" style={{width:250,height:200}}></img>
-          <img src="./Images/Gallery/gallery-img-3.jpg" alt="" style={{width:250,height:200}}></img>
-          <img src="./Images/Gallery/gallery-img-4.jpg" alt="" style={{width:250,height:200}}></img>
-          <img src="./Images/Gallery/gallery-img-5.jpg" alt="" style={{width:250,height:200}}></img>
-          <img src="./Images/Gallery/gallery-img-6.jpg" alt="" style={{width:250,height:200}}></img>
-          <img src="./Images/Gallery/gallery-img-7.jpg" alt="" style={{ width: 250, height: 200 }}></img>
-          <img src="./Images/Gallery/gallery-img-8.jpg" alt="" style={{width:250,height:200}}></img>
-          <img src="./Images/Gallery/gallery-img-9.jpg" alt="" style={{width:250,height:200}}></img>
-          <img src="./Images/Gallery/gallery-img-10.jpg" alt="" style={{width:250,height:200}}></img>
-          <img src="./Images/Gallery/gallery-img-11.jpg" alt="" style={{width:250,height:200}}></img>
-          <img src="./Images/Gallery/gallery-img-12.jpg" alt="" style={{width:250,height:200}}></img>
+      <div className='container mt-5  '>
+        <div className='row d-flex gap-2 flex-row  justify-content-center   box'>
+          <img className='pb-4 ' src="./Images/Gallery/gallery-img-1.jpg" alt="" style={{ width: 265, height: 230 }}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-2.jpg" alt="" style={{ width: 265, height: 230}}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-3.jpg" alt="" style={{ width: 265, height: 230}}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-4.jpg" alt="" style={{ width: 265, height: 230}}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-5.jpg" alt="" style={{ width: 265, height: 230}}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-6.jpg" alt="" style={{ width: 265, height: 230}}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-7.jpg" alt="" style={{ width: 265, height: 230 }}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-8.jpg" alt="" style={{ width: 265, height: 230}}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-9.jpg" alt="" style={{ width: 265, height: 230}}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-10.jpg" alt="" style={{ width: 265, height: 230}}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-11.jpg" alt="" style={{ width: 265, height: 230}}></img>
+          <img className='pb-4' src="./Images/Gallery/gallery-img-12.jpg" alt="" style={{ width: 265, height: 230}}></img>
           
           
         </div>
