647b2266afcbc6f6c882945f325b30b00c244b20
MuhammadAdnan11 <adnan191022@gmail.com>
2023-02-25 10:22:41 +0500

    updated pages

--- project/src/Components/error_page.js
+++ project/src/Components/error_page.js
@@ -4,21 +4,6 @@ import "./error_page.css";
 const error_page = () => {
   return (
     <>
-      {/* <div className="error_pic">
-        <img src="./Images/error-404/404-background.png" alt="" />
-        <div className="icon">
-          <img src="./Images/error-404/404-image.png" alt="" />
-          <h4>
-            Oops, seems you ventured to far out
-            <br /> into space. Here's the way back<center> home.</center>
-          </h4>
-              </div>
-              <div className="button">
-              <Button variant="primary rounded-pill">BACK TO HOME</Button>
-        </div>
-      </div> */}
-
-
       <div className="container-fluid  bgimg">
         <div className="container">
           <div className="row align-self-center justify-content-center ">
