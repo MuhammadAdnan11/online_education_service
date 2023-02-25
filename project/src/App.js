9e051673f4a6bc52add10bcb89519130f956b4ed
MuhammadAdnan11 <adnan191022@gmail.com>
2023-02-25 10:22:11 +0500

    updated pages

--- project/src/App.js
+++ project/src/App.js
@@ -2,8 +2,8 @@
 import React from 'react';
 // import Navbar from './Components/Navbar';
 // import Home from "./Components/Home"
-import Gallery from './Components/Gallery';
-// import Error_page from './Components/error_page';
+// import Gallery from './Components/Gallery';
+import Error_page from './Components/error_page';
 
 // import Faq from './Components/Faq';
 // import Contact from './Components/contactUs';
@@ -18,8 +18,8 @@ function App() {
    
 {/* <Navbar/>
 <Home/> */}
-      <Gallery/>
-      {/* <Error_page/> */}
+      {/* <Gallery/> */}
+      <Error_page/>
       {/* <Faq /> */}
       {/* <Contact/> */}
       {/* <Cart /> */}
--- project/src/Components/error_page.js
+++ project/src/Components/error_page.js
@@ -4,16 +4,30 @@ import "./error_page.css";
 const error_page = () => {
   return (
     <>
-      
+      {/* <div className="error_pic">
+        <img src="./Images/error-404/404-background.png" alt="" />
+        <div className="icon">
+          <img src="./Images/error-404/404-image.png" alt="" />
+          <h4>
+            Oops, seems you ventured to far out
+            <br /> into space. Here's the way back<center> home.</center>
+          </h4>
+              </div>
+              <div className="button">
+              <Button variant="primary rounded-pill">BACK TO HOME</Button>
+        </div>
+      </div> */}
+
+
       <div className="container-fluid  bgimg">
         <div className="container">
           <div className="row align-self-center justify-content-center ">
-            <img src="./Images/error-404/404-image.png" alt="" style={{ width: 400, height: 300 }} className="mt-5"></img>
-            <p className="text-center text-white fs-3 fw-bolder">Oops, seems you ventured to far out<br/>
+            <img  src="./Images/error-404/404-image.png" alt="" style={{ width: 459, height: 178 }} className="mt-5"></img>
+            <p className="text-center text-white fs-4 fw-bolder mt-4">Oops, seems you ventured to far out<br/>
               into space. Here's the way back<br/>
               home.</p>
             <di className="text-center">
-            <button className="btn btn-primary rounded-pill text-center bg-white text-black">BACK TO HOME</button>
+            <button className="rounded-pill text-center bg-white fw-bold mt-3 " id="btnstyle" style={{height:"47px", width:"200px"}}>BACK TO HOME</button>
             </di>
           </div>
         
