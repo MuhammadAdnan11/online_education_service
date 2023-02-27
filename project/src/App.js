08e4b2d7c6a184596bd5619acc3b47a849e160ff
MuhammadAdnan11 <adnan191022@gmail.com>
2023-02-27 09:52:56 +0500

    ok

--- project/src/App.js
+++ project/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import Navbar from './Components/Navbar';
 // import Home from "./Components/Home"
 // import Gallery from './Components/Gallery';
-import Error_page from './Components/error_page';
+// import Error_page from './Components/error_page';
 
 // import Faq from './Components/Faq';
 // import Contact from './Components/contactUs';
@@ -19,7 +19,7 @@ function App() {
 {/* <Navbar/>
 <Home/> */}
       {/* <Gallery/> */}
-      <Error_page/>
+      {/* <Error_page/> */}
       {/* <Faq /> */}
       {/* <Contact/> */}
       {/* <Cart /> */}
