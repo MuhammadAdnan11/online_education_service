4fc7b75cb0f40b00bbf1aa46f6b25adb2bf18d5d
MuhammadAdnan11 <adnan191022@gmail.com>
2023-03-11 12:31:16 +0500

    ok

--- project/src/Components/InstructorCard.js
+++ project/src/Components/InstructorCard.js
@@ -1,34 +1,14 @@
 import React from "react";
 import "./Instructor.css";
 import Card from 'react-bootstrap/Card';
-import {BiTimeFive} from "react-icons/bi"
-import {FiMapPin} from "react-icons/fi"
+
 
 
 
 const InstructorCard = (props) => {
   return (
     <>
-      {/* <div className="container ">
-        <div className="row d-flex gap-4 flex-row pb-4 justify-content-center ">
-          <div className="col d-flex justify-content-evenly ">
-              <div className="text-center pt-4">
-              <a href="#">
-                <img src={props.imginsrc} style={{ width: 80, height: 80 }} />
-              </a>
-              </div>
-            <div
-              className="col"
-              style={{ height: "110px", width: "240px" }}
-            >
-              <a href="#">
-                              <h5 className="fw-bold">{props.inname }</h5>
-              </a>
-                          <p>{ props.instr}</p>
-            </div>
-          </div>
-        </div>
-      </div> */}
+    
           
 
           <Card style={{ width: '20rem',height:"102px" }} className="d-flex flex-row HoverDiv" id="intShadow">
