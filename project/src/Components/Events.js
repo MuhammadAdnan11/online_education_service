f9b84609898fb3c515741ac7bc20462773fa4555
Muhammad Adnan <adnan191022@gmail.com>
2023-06-17 18:58:55 +0500

    updated

--- project/src/Components/Events.js
+++ project/src/Components/Events.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import Navbar from './Navbar';
+import { Link } from 'react-router-dom';
+const gallerybackimage=require('../AssetsFolder/Gallery/title-area-background.png');
+const gallerybg={
+  width: '100%',
+    height: '35vh',
+  backgroundImage: `url(${gallerybackimage})`,
+  backgroundSize:'cover'
+};
+
+
 
 const Events = () => {
   return (
       <>
       <Navbar />
-      <div className='container-fluid color-blue'>
-        <div>
-          <h1>Welcome back</h1>
-       </div>
-     </div>
+      <div className='container-fluid' style={gallerybg}>
+
+                <div className='container'>
+                    <div className='row'>
+                    <h1 className='fw-bolder text-white mt-5 pt-3  hcard' >Events Calendar</h1>
+                    <div className='d-flex flex-row gap-2'>
+                  <span className='text-white'><Link to="/" className="text-white">Home</Link></span> 
+                {/* <span className='text-white'> /</span>
+               <span className='text-white'><Link to="/courses" className="text-white">All Courses</Link></span> */}
+              </div>
+                    </div>
+                    
+                </div>
+         
+            </div>
+
       </>
   )
 }
