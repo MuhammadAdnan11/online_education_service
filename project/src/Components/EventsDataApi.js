f27a7198b64e27109c30f412496a5fb758d8108e
Muhammad Adnan <adnan191022@gmail.com>
2023-06-17 21:56:43 +0500

    api

---
+++ project/src/Components/EventsDataApi.js
@@ -0,0 +1,11 @@
+export default[
+    {
+        "id": 1,
+        "name": "Team Building",
+        "date": "17.01.2024 @ 9:00 am",
+        "time": "9:00 am - 9:00 am",
+        "venue": "Harward Education",
+        "address": "Cambridge, MA 04245, USA",
+        "details": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. "
+    }
+]
